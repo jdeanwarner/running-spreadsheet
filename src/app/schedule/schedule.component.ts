4c579a12b2f64aaf86120dd86cd1a3e30d94b3c5
Jordan Warner <warnjor@mysdhc.net>
2019-02-07 11:37:09 -0500

    default season show should now be 1 - first current active seasn, 2 - season with start date closest to the current date

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -24,11 +24,50 @@ export class ScheduleComponent implements OnInit {
         if (params.seasonId) {
           this.activeSeason = this.seasons.filter((season: Season) => season.id === params.seasonId)[0];
         } else {
-          this.activeSeason = this.seasons[0];
+          this.activeSeason = this.getActiveSeason(seasons);
         }
-        this.activityService.getScheduledActivities(this.activeSeason.id).subscribe(result => console.log(result));
       });
     });
+  }
+
+  getActiveSeason(seasons: Season[]): Season {
+    let activeSeason: Season = seasons.filter((season: Season) => {
+      const startDate = this.getStartDate(season);
+      const endDate = this.getEndDate(season);
+      if (new Date(startDate) < new Date() && new Date(endDate) > new Date()) {
+        return season;
+      }
+    })[0];
+
+    if (!activeSeason) {
+      activeSeason = seasons.sort((season1: Season, season2: Season) => {
+        if (Math.abs(this.getStartDate(season1) - new Date().getTime()) > Math.abs(this.getStartDate(season2) - new Date().getTime())) {
+          return -1;
+        }
+        if (Math.abs(this.getStartDate(season1) - new Date().getTime()) < Math.abs(this.getStartDate(season2) - new Date().getTime())) {
+          return 1;
+        }
+        return 0;
+      })[0];
+    }
+
+    return activeSeason;
+  }
+
+  getStartDate(season: Season) {
+    if (season && season.trainingBlocks) {
+      return Math.min(...season.trainingBlocks.map(block => block.startDate.toDate().getTime()));
+    } else {
+      return 0;
+    }
+  }
+
+  getEndDate(season: Season) {
+    if (season && season.trainingBlocks) {
+      return Math.max(...season.trainingBlocks.map(block => block.endDate.toDate().getTime()));
+    } else {
+      return 0;
+    }
   }
 
   addSeason(season: Season): void {
