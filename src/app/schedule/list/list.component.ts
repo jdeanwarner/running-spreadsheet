55f5573ed60d088272f6391261ba618a15584ade
Jordan Warner <warnjor@mysdhc.net>
2019-02-07 11:09:22 -0500

    seasons list is now sorted by start date newest first

--- src/app/schedule/list/list.component.ts
+++ src/app/schedule/list/list.component.ts
@@ -10,13 +10,37 @@ import { MatDialog } from '@angular/material';
 })
 export class ListComponent implements OnInit {
 
-  @Input() seasons: Season[];
+  _seasons: Season[];
+  @Input() set seasons(seasons: Season[]) {
+    if (seasons) {
+      this._seasons = seasons.sort((season1: Season, season2: Season) => {
+        const season1StartDate = this.getSeasonStartDate(season1);
+        const season2StartDate = this.getSeasonStartDate(season2);
+        if (season1StartDate > season2StartDate) {
+          return -1;
+        }
+        if (season1StartDate < season2StartDate) {
+          return 1;
+        }
+        return 0;
+      });
+    }
+  }
   @Output() addSeason: EventEmitter<Season> = new EventEmitter<Season>();
   @Output() selected: EventEmitter<string> = new EventEmitter<string>();
 
   constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
+
+  }
+
+  getSeasonStartDate(season: Season): number {
+    if (season && season.trainingBlocks) {
+      return Math.max(...season.trainingBlocks.map(block => block.startDate.seconds));
+    } else {
+      return 0;
+    }
   }
 
   add() {
