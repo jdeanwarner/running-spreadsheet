a9ef72cde13888303007d8f3f55e22956d803678
Jordan Warner <jordan.warner@sdhc.k12.fl.us>
2019-11-13 14:44:17 -0500

    updated add goal component. not done yet

--- src/app/goals/add-goal/add-goal.component.ts
+++ src/app/goals/add-goal/add-goal.component.ts
@@ -1,3 +1,7 @@
+import { Month } from './../../shared/month.enum';
+import { GoalType } from './../goal-type.enum';
+import { GoalPeriod } from './../goal-period.enum';
+import { FormGroup, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddGoalComponent implements OnInit {
 
+  formGroup: FormGroup = new FormGroup({
+    id: new FormControl(),
+    goalPeriod: new FormControl(),
+    type: new FormControl(),
+    value: new FormControl(),
+    month: new FormControl(),
+    year: new FormControl()
+  });
+
+  GOAL_PERIOD = GoalPeriod;
+  GOAL_TYPE = GoalType;
+
+  goalPeriodArray = Object.keys(GoalPeriod).map(k => ({key: k, value: GoalPeriod[k]}));
+  goalTypeArray = Object.keys(GoalType).map(k => ({key: k, value: GoalType[k]}));
+  monthArray = Object.keys(Month).map(k => ({key: k, value: Month[k]}));
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  delete() {
+
+  }
+
+  save() {
+
+  }
+
+  close() {
+
   }
 
 }
---
+++ src/app/goals/goal-period.enum.ts
@@ -0,0 +1,4 @@
+export enum GoalPeriod {
+    MONTH = 'Month',
+    YEAR = 'Year'
+}
