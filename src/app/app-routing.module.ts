36c90b85788fe3257ffb4194d43fc1016df8d2f4
Jordan Warner <jordan.warner@sdhc.k12.fl.us>
2019-11-19 11:31:42 -0500

    added route guard to check if logged in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './shared/guards/auth.guard';
 
 const appRoutes: Routes = [
     {
       path: 'goals',
-      loadChildren: () => import('./goals/goals.module').then(m => m.GoalsModule)
+      loadChildren: () => import('./goals/goals.module').then(m => m.GoalsModule),
+      canActivate: [ AuthGuard ]
     },
     {
       path: 'schedule',
-      loadChildren: () => import('./schedule/schedule.module').then(m => m.ScheduleModule)
+      loadChildren: () => import('./schedule/schedule.module').then(m => m.ScheduleModule),
+      canActivate: [ AuthGuard ]
     },
     {
       path: 'races',
-      loadChildren: () => import('./races/races.module').then(m => m.RacesModule)
+      loadChildren: () => import('./races/races.module').then(m => m.RacesModule),
+      canActivate: [ AuthGuard ]
     },
     {
       path: 'log',
-      loadChildren: () => import('./run-log/run-log.module').then(m => m.RunLogModule)
+      loadChildren: () => import('./run-log/run-log.module').then(m => m.RunLogModule),
+      canActivate: [ AuthGuard ]
     },
     { path: '',
       redirectTo : '/log',
